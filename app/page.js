d9a5073ebab296076a9ec724e05f8f43b44b3c17
Christos Dematas <demataschristiano@gmail.com>
2023-08-11 20:21:30 +0300

    progress, 1h 10mins in

--- app/page.js
+++ app/page.js
@@ -1,95 +1,94 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <nav className={styles.nav}>
+        <div className="">#LIVEINGREECE</div>
+        <div className={styles.links}>
+          <div>For</div>
+          <div>TALENTS</div>
+          <div>ENTREPRENEURS</div>
+          <div>INVESTORS</div>
+          <div className={styles.extraLinks}>
+            <div>Innitiatives</div>
+            <div>Q&A</div>
+          </div>
         </div>
-      </div>
+      </nav>
+      <main className={styles.main}>
+        <div className={styles.sectionOne}>
+          <h1 className={styles.header}>
+            Greece <br />
+            the <br />
+            birthplace <br />
+            of ideas
+          </h1>
+          <Image
+            src="/assets/img/hero-img-01.png"
+            alt="Vercel Logo"
+            className={styles.heroImg}
+            width={445}
+            height={445}
+            priority
+          />
+          <Image
+            src="/assets/img/hero-img-02.png"
+            alt="Vercel Logo"
+            className={styles.heroImg}
+            width={445}
+            height={445}
+            priority
+          />
+        </div>
+        <div className={styles.sectionOne}>
+          <Image
+            src="/assets/img/hero-img-03.png"
+            alt="Vercel Logo"
+            className={styles.heroImg}
+            width={445}
+            height={445}
+            priority
+          />
+          <div className={styles.sectionMiddle}>
+            <p className={styles.paragraph}>
+              Right in the cradle of Western civilization and at the heart of
+              contemporary innovation, we invite innovative minds,
+              ecosystem-driving entrepreneurs and leading investors to explore,
+              grow, and invest in a country that blends rich history,
+              captivating culture and a promising entrepreneurial ecosystem.
+            </p>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <button className={styles.sectionMiddleButton}>
+              Become a citizen{" "}
+              <Image
+                src="/assets/img/arrowrighttop.png"
+                alt="Vercel Logo"
+                className={styles.heroImg}
+                width={16}
+                height={16}
+                priority
+              />
+            </button>
+          </div>
+          <Image
+            src="/assets/img/hero-img-04.png"
+            alt="Vercel Logo"
+            className={styles.heroImg}
+            width={445}
+            height={445}
+            priority
+          />
+        </div>
+        <div className={styles.sectionTwo}>
+          <h1 className={styles.header}>
+            Discover innovation <br />
+            opportunities
+          </h1>
+          <div>accordions sections ?</div>
+        </div>
+      </main>
+    </>
+  );
 }
